5262ca681d9a16d6733ced120d61b8a3108fca6b
atlanteavila <atlanteavila@gmail.com>
2016-07-20 15:17:57 -0700

    Added dynamic port numbers.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+// var port = 4568;
+var port_number = server.listen(process.env.PORT || 3000);
+app.listen(port_number);
 
 app.listen(port);
 
