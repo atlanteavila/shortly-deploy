680ef24d194588b70dbe87e9b67c70b01a333d34
atlanteavila <atlanteavila@gmail.com>
2016-07-20 15:43:49 -0700

    corrected

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
 
 // var port = 4568;
-var port_number = server.listen(process.env.PORT);
+var port_number = process.env.PORT || 3000;
 app.listen(port_number);
+
+//app.listen(port);
 
 console.log('Server now listening on port ' + port);
